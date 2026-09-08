e8c4afd8f6c11937559c41cc6c91781462656aa0
hyzhuimeng <466457178@qq.com>
2026-09-08 18:11:26 +0800

    344

---
+++ algorithm/344.py
@@ -0,0 +1,9 @@
+def reverseString(s:list[str]) -> None:
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        s[left],s[right] = s[right],s[left]
+        left+=1
+        right-=1
+        return s
+print(reverseString(["h","e","l","l","o"]))
